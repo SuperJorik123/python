b2d33849c7a371faec088f28d5748b39721d84b5
User <victorbaxan111@gmail.com>
2022-06-27 19:33:39 +0300

    first commit

---
+++ Python for begginers/Project 1/basics.py
@@ -0,0 +1,6 @@
+weekly_temp = [10.0, 7.7, 3.5, 8.9]
+students_grades = {"Victor": 7.5, "Vasea": 10.0,"Cristi": 9.2}
+mysum = sum(students_grades.values())
+gr_num = len(students_grades)
+avg_gr = mysum / gr_num
+print(avg_gr)
